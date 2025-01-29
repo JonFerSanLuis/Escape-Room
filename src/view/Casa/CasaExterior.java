ee1beaf828c4e382dac9397db0bd96eb9a958de4
Aldo Arrieta <aldo.dayron81@gmail.com>
2025-01-29 19:50:04 +0100

    CasaXFuera Actualizado

--- src/view/Casa/CasaExterior.java
+++ src/view/Casa/CasaExterior.java
@@ -26,95 +26,67 @@ public class CasaExterior extends JFrame {
 
 	public CasaExterior() {
 		// Configuración básica del JFrame
-		setTitle("CasaExImagen");
+		setTitle("Casa");
 		setSize(1000, 533);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+		setResizable(false);
 		getContentPane().setLayout(null);
+		
+		JLabel lblPostit = new JLabel();
+		lblPostit.setIcon(new ImageIcon("img/PostitEntrada.png"));
+		lblPostit.setBounds(281, 311, 46, 50);
+		getContentPane().add(lblPostit);
+		
+		JButton btnPosit = new JButton("");
+
+		btnPosit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblPostit.setIcon(new ImageIcon("img/PostitClick.png"));
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblPostit.setIcon(new ImageIcon("img/PostitEntrada.png"));
+			}
+
+		});
+		
+		btnPosit.setBounds(281, 311, 46, 50);
+		btnPosit.setOpaque(false);
+		btnPosit.setBackground(new Color(0, 0, 0, 0));
+		btnPosit.setBorderPainted(false); // No dibujar el borde
+		btnPosit.setFocusPainted(false);
+		btnPosit.setContentAreaFilled(false);
+		getContentPane().add(btnPosit);
 
 		// Imagen pequeña 1
-		String smallImagePath1 = "D:/Josué/Reto2/ImagenesESR/PistaIcono.png";
+		String smallImagePath1 = "img/PistaIcono.png";
 		ImageIcon smallScaledIcon1 = new ImageIcon(
 				new ImageIcon(smallImagePath1).getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
 
 		// Imagen pequeña 2
-		String smallImagePath2 = "D:/Josué/Reto2/ImagenesESR/MochilaIcono.png";
+		String smallImagePath2 = "img/MochilaIcono.png";
 		ImageIcon smallScaledIcon2 = new ImageIcon(
 				new ImageIcon(smallImagePath2).getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
 
 		// Imagen de fondo
-		String imagePath = "D:/Josué/Reto2/ImagenesESR/CasaXFuera.jpg";
+		String imagePath = "img/CasaExteriorImg.jpeg";
 		ImageIcon scaledIcon = new ImageIcon(
 				new ImageIcon(imagePath).getImage().getScaledInstance(1000, 500, Image.SCALE_SMOOTH));
-						
-		JLabel lblPostit = new JLabel();
-		lblPostit.setIcon(new ImageIcon("D:\\Josu\u00E9\\Mi Seminario\\Proyecto\\Escape-Room\\img\\PostitEntrada.png"));
-		lblPostit.setBounds(281, 311, 46, 50);
-		getContentPane().add(lblPostit);
-		;
-						lblPostit.addMouseListener(new MouseAdapter() {
-							@Override
-							public void mouseEntered(MouseEvent e) {
-								lblPostit.setIcon(new ImageIcon("C:\\Users\\Alumno\\Downloads\\PostitClick.png"));
 
-							}
-							
-							
-							@Override
-							public void mouseExited(MouseEvent e) {
-								lblPostit.setIcon(new ImageIcon("C:\\Users\\Alumno\\Downloads\\PostitEntrada.png"));
-							}
-							
-						});
-						
-				
-						// Imagen pequeña 2
-						JLabel lblMochila = new JLabel(new ImageIcon("D:\\Josué\\Mi Seminario\\Proyecto\\Escape-Room\\img\\IconoMochilaEX.png"));
-						lblMochila.addMouseListener(new MouseAdapter() {
-						    @Override
-						    public void mouseEntered(MouseEvent e) {
-						        lblMochila.setIcon(new ImageIcon("D:\\Josué\\Mi Seminario\\Proyecto\\Escape-Room\\img\\IconoMochilaEX.png"));
-						    }
-						    @Override
-						    public void mouseExited(MouseEvent e) {
-						        lblMochila.setIcon(new ImageIcon("D:\\Josué\\Mi Seminario\\Proyecto\\Escape-Room\\img\\IconoMochilaENT.png"));
-						    }
-						});
-
-						lblMochila.setBounds(835, 11, 65, 60); // Posición adicional a la derecha del JFrame
-						getContentPane().add(lblMochila);
-		
-				// Imagen pequeña 1
-				JLabel lblPista = new JLabel(new ImageIcon("D:\\Josu\u00E9\\Mi Seminario\\Proyecto\\Escape-Room\\img\\IconoPistaEX.png"));
-				lblPista.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseEntered(MouseEvent e) {
-						lblPista.setIcon(new ImageIcon("D:\\Josué\\Mi Seminario\\Proyecto\\Escape-Room\\img\\IconoPistaENT.png"));
-					}
-					@Override
-					public void mouseExited(MouseEvent e) {
-						lblPista.setIcon(new ImageIcon("sources (in img) [Escape-Room master]\\IconoPistaEX.png"));
-					}
-				});
-				lblPista.setBounds(910, 11, 64, 60); // Posición en la parte derecha del JFrame
-				getContentPane().add(lblPista);
-
-		JLabel lblReaccionPuertaCasa = new JLabel();
-		lblReaccionPuertaCasa.setForeground(Color.WHITE);
-		lblReaccionPuertaCasa.setBounds(272, 404, 510, 50);
-		getContentPane().add(lblReaccionPuertaCasa);
-
-		// Añadir la imagen de fondo primero
-		JLabel lblCasa = new JLabel(scaledIcon);
-		lblCasa.setBounds(0, -52, 984, 602);
-		lblCasa.setHorizontalAlignment(SwingConstants.CENTER);
-		getContentPane().add(lblCasa);
 
 		// Botón (btnPuertaCasa)
 		JButton btnPuertaCasa = new JButton();
 		btnPuertaCasa.setBounds(467, 268, 67, 106);
 		btnPuertaCasa.setOpaque(false);
 		getContentPane().add(btnPuertaCasa);
+
+		JLabel lblReaccionPuertaCasa = new JLabel();
+		lblReaccionPuertaCasa.setForeground(Color.WHITE);
+		lblReaccionPuertaCasa.setBounds(272, 404, 510, 50);
 
 		btnPuertaCasa.addActionListener(new ActionListener() {
 			@Override
@@ -130,6 +102,50 @@ public class CasaExterior extends JFrame {
 				}
 			}
 		});
+		
+		getContentPane().add(lblReaccionPuertaCasa);
+		btnPuertaCasa.setVisible(false);
+
+
+		// Imagen pequeña 2
+		JLabel lblMochila = new JLabel(new ImageIcon("img/IconoMochilaEX.png"));
+		lblMochila.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblMochila.setIcon(new ImageIcon("img/IconoMochilaENT.png"));
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblMochila.setIcon(new ImageIcon("img/IconoMochilaEX.png"));
+			}
+		});
+
+		lblMochila.setBounds(835, 11, 65, 60); // Posición adicional a la derecha del JFrame
+		getContentPane().add(lblMochila);
+
+		// Imagen pequeña 1
+		JLabel lblPista = new JLabel(new ImageIcon("img/IconoPistaEX.png"));
+		lblPista.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblPista.setIcon(new ImageIcon("img/IconoPistaENT.png"));
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblPista.setIcon(new ImageIcon("img/IconoPistaEX.png"));
+			}
+		});
+		lblPista.setBounds(910, 11, 64, 60); // Posición en la parte derecha del JFrame
+		getContentPane().add(lblPista);
+
+
+		// Añadir la imagen de fondo primero
+		JLabel lblCasa = new JLabel(scaledIcon);
+		lblCasa.setBounds(0, -52, 984, 602);
+		lblCasa.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblCasa);
 
 	}
 

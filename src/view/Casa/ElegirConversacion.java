5e8b706ac9e87c5a6e17b38b6802a89b57d7137e
ErlantzSanluis <tolentinoerla@gmail.com>
2025-02-11 09:19:58 +0100

    Flecha Atras

--- src/view/Casa/ElegirConversacion.java
+++ src/view/Casa/ElegirConversacion.java
@@ -21,72 +21,89 @@ import java.awt.event.MouseEvent;
 
 public class ElegirConversacion extends JFrame {
 
-	private JPanel contentPane;
-	private JLabel lblTv;
-	private JTextField textField;
-	private JButton btnSubmit;
-	private int attemptCount = 0;  // Contador de intentos
-	private JButton btnConver2;
-	private JLabel lblfotoconver1;
-	private JLabel lblfotoconver2;
+    private JPanel contentPane;
+    private JLabel lblTv;
+    private JTextField textField;
+    private JButton btnSubmit;
+    private int attemptCount = 0;
+    private JButton btnConver2;
+    private JLabel lblfotoconver1;
+    private JLabel lblfotoconver2;
+    private JButton btnVolver;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ElegirConversacion frame = new ElegirConversacion();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ElegirConversacion frame = new ElegirConversacion();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public ElegirConversacion() {
-		setResizable(false);
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    setBounds(200, 100, 950, 600);
+    public ElegirConversacion() {
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(200, 100, 950, 600);
 
-	    contentPane = new JPanel();
-	    setContentPane(contentPane);
-	    contentPane.setLayout(null);
+        contentPane = new JPanel();
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-	    String backgroundFile = "img/Fototelevision -conver.jpg";
-	    ImageIcon originalIcon = new ImageIcon(backgroundFile);
-	    Image originalImage = originalIcon.getImage();
-	    Image scaledImage = originalImage.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
-	    ImageIcon scaledIcon = new ImageIcon(scaledImage);
-	    
+        String backgroundFile = "img/Fototelevision -conver.jpg";
+        ImageIcon originalIcon = new ImageIcon(backgroundFile);
+        Image originalImage = originalIcon.getImage();
+        Image scaledImage = originalImage.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
         lblfotoconver1 = new JLabel();
         lblfotoconver1.setBounds(332, 72, 309, 389);
         String imgPath = "D:\\Erlantz\\Programacion\\Escape-Room\\img\\movilconver1 (1).png";
         ImageIcon imgIcon = new ImageIcon(imgPath);
-        lblfotoconver1.setVisible(false);  // Oculta la imagen al inicio
+        lblfotoconver1.setVisible(false);
         Image img = imgIcon.getImage().getScaledInstance(lblfotoconver1.getWidth(), lblfotoconver1.getHeight(), Image.SCALE_SMOOTH);
+        
+                // Botón de volver
+                btnVolver = new JButton("");
+                btnVolver.setBounds(50, 500, 80, 50);
+                ImageIcon iconVolver = new ImageIcon("img\\FlechaAtrasTV.png");
+                Image imgVolver = iconVolver.getImage().getScaledInstance(80, 50, Image.SCALE_SMOOTH);
+                btnVolver.setIcon(new ImageIcon(imgVolver));
+                btnVolver.setVisible(false);
+                btnVolver.setOpaque(false);
+                btnVolver.setContentAreaFilled(false);
+                btnVolver.setBorderPainted(false);
+                btnVolver.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        lblfotoconver1.setVisible(false);
+                        lblfotoconver2.setVisible(false);
+                        btnVolver.setVisible(false);
+                    }
+                });
+                
+                        contentPane.add(btnVolver);
         lblfotoconver1.setIcon(new ImageIcon(img));
         contentPane.add(lblfotoconver1);
-        
+
+        String imgPath2 = "D:\\Erlantz\\Programacion\\Escape-Room\\img\\MovilConver2.png";
+        ImageIcon imgIcon2 = new ImageIcon(imgPath2);
+
         lblfotoconver2 = new JLabel();
-        lblfotoconver2.setBounds(332, 72, 309, 389);
-        String imgPath1 = "D:\\Erlantz\\Programacion\\Escape-Room\\img\\MovilConver2.png";
-        ImageIcon imgIcon2 = new ImageIcon(imgPath1);
-        lblfotoconver2.setVisible(false);  // Oculta la imagen al inicio
+        lblfotoconver2.setBounds(298, 33, 395, 485);
+        lblfotoconver2.setVisible(false);
         Image img2 = imgIcon2.getImage().getScaledInstance(lblfotoconver2.getWidth(), lblfotoconver2.getHeight(), Image.SCALE_SMOOTH);
-        lblfotoconver2.setIcon(new ImageIcon(img));
+        lblfotoconver2.setIcon(new ImageIcon(img2));
         contentPane.add(lblfotoconver2);
-	    
-	    lblTv = new JLabel();
-	    lblTv.setBounds(0, 0, 944, 561);
-	    lblTv.setIcon(scaledIcon);
-	    contentPane.add(lblTv);
-        
+
+        lblTv = new JLabel();
+        lblTv.setBounds(0, 0, 944, 561);
+        lblTv.setIcon(scaledIcon);
+        contentPane.add(lblTv);
+
         btnConver2 = new JButton("");
         btnConver2.setBounds(497, 190, 208, 141);
         contentPane.add(btnConver2);
@@ -97,23 +114,23 @@ public class ElegirConversacion extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 lblfotoconver2.setVisible(true);
+                btnVolver.setVisible(true);
             }
         });
-        
+
         JButton btnConver1 = new JButton("");
         btnConver1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 lblfotoconver1.setVisible(true);
+                btnVolver.setVisible(true);
             }
         });
-
 
         btnConver1.setBounds(259, 190, 208, 141);
         contentPane.add(btnConver1);
         btnConver1.setOpaque(false);
         btnConver1.setContentAreaFilled(false);
         btnConver1.setBorderPainted(false);
-        
-	}
+    }
 }

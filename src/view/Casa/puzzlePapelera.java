20b8c45e24572db558d2853c397422c3b6c40c29
Aldoo81 <aldo.dayron81@gmail.com>
2025-02-13 13:59:40 +0100

    puzzlePapelera

--- src/view/Casa/puzzlePapelera.java
+++ src/view/Casa/puzzlePapelera.java
@@ -10,11 +10,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import java.util.Random;
 
 public class puzzlePapelera extends JFrame {
 
     private JPanel contentPane;
-    private JLabel[] piezas = new JLabel[10];
+    private JLabel pieza1, pieza2, pieza3, pieza4, pieza5, pieza6, pieza7, pieza8, pieza9, pieza10;
     private Point mouseOffset;
 
     public static void main(String[] args) {
@@ -39,25 +40,50 @@ public class puzzlePapelera extends JFrame {
         
         // Botón para mostrar los labels
         JButton btnMostrar = new JButton("Mostrar Piezas");
-        btnMostrar.setBounds(10, 10, 150, 30);
+        btnMostrar.setBounds(10, 10, 150, 70);
         btnMostrar.addActionListener(e -> {
-            for (JLabel pieza : piezas) {
-                pieza.setVisible(true);
-            }
+            pieza1.setVisible(true);
+            pieza2.setVisible(true);
+            pieza3.setVisible(true);
+            pieza4.setVisible(true);
+            pieza5.setVisible(true);
+            pieza6.setVisible(true);
+            pieza7.setVisible(true);
+            pieza8.setVisible(true);
+            pieza9.setVisible(true);
+            pieza10.setVisible(true);
         });
         contentPane.add(btnMostrar);
         
-        // Crear y agregar las piezas
-        for (int i = 0; i < piezas.length; i++) {
-            piezas[i] = new JLabel("Pieza " + (i + 1));
-            piezas[i].setBounds(50 + (i * 60), 100, 80, 30); // Posición inicial
-            piezas[i].setVisible(false); // Invisibles al inicio
-            piezas[i].setBorder(new LineBorder(Color.BLACK));
-            DragListener dragListener = new DragListener();
-            piezas[i].addMouseListener(dragListener);
-            piezas[i].addMouseMotionListener(dragListener);
-            contentPane.add(piezas[i]);
-        }
+        // Crear y agregar las piezas manualmente
+        pieza1 = createPieza("Pieza 1");
+        pieza2 = createPieza("Pieza 2");
+        pieza3 = createPieza("Pieza 3");
+        pieza4 = createPieza("Pieza 4");
+        pieza5 = createPieza("Pieza 5");
+        pieza6 = createPieza("Pieza 6");
+        pieza7 = createPieza("Pieza 7");
+        pieza8 = createPieza("Pieza 8");
+        pieza9 = createPieza("Pieza 9");
+        pieza10 = createPieza("Pieza 10");
+    }
+    
+    private JLabel createPieza(String texto) {
+        JLabel pieza = new JLabel(texto);
+        pieza.setVisible(false);
+        pieza.setBorder(new LineBorder(Color.BLACK));
+        DragListener dragListener = new DragListener();
+        pieza.addMouseListener(dragListener);
+        pieza.addMouseMotionListener(dragListener);
+        contentPane.add(pieza);
+
+        // Desordenar las posiciones horizontalmente y ponerlas cerca de la parte inferior
+        Random rand = new Random();
+        int x = 500 - rand.nextInt(200);  // Genera un valor aleatorio para la posición X (ancho de la ventana)
+        int y = 400 - rand.nextInt(50);  // Genera un valor aleatorio para la posición Y (parte inferior)
+        pieza.setBounds(x, y, 80, 80);
+
+        return pieza;
     }
     
     private class DragListener extends MouseAdapter {
@@ -73,4 +99,6 @@ public class puzzlePapelera extends JFrame {
             label.setLocation(newLocation);
         }
     }
-} 
+}
+
+
